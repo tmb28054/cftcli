81cee990a58e3212f582dd47a94751ef3f2da0d9
Topaz M. Bott <topaz@topazhome.net>
2022-10-19 20:45:40 -0400

    stuff

--- scripts/push-github.sh
+++ scripts/push-github.sh
@@ -3,6 +3,9 @@
 set -e
 
 if [ "${CI_COMMIT_BRANCH}" == "main" ]; then
-    git remote add github git@github.com:tmb28054/cftcli.git
-    git push -u github main
+  git clone git@gitlab.botthouse.net:botthouse/cftcli.git gitlab
+  cd gitlab
+  git remote add github git@github.com:tmb28054/cftcli.git
+  git push -u github main
 fi
+
