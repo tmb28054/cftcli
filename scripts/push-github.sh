93abca8eace88cba32b6485426d21cccc2850a88
Topaz M. Bott <topaz@topazhome.net>
2022-10-24 07:27:44 -0400

    fix

--- scripts/push-github.sh
+++ scripts/push-github.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 if [ "${CI_COMMIT_BRANCH}" == "main" ]; then
+  echo "ensuring local clone does not exist"
+  test -e gitlab && rm -rfv gitlab
+  echo "cloning from gitlab"
   git clone git@gitlab.botthouse.net:botthouse/cftcli.git gitlab
   cd gitlab
+  echo "adding github repo"
   git remote add github git@github.com:tmb28054/cftcli.git
+  echo "pushing to github"
   git push -u github main
 fi
-
